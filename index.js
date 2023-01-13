6f014e8de2a40c8e3a27f121f7861a4fa1f8019a
Luke Leber <lal65@psu.edu>
2023-01-13 17:16:07 -0500

    Initial commit for fallback BLS api provider...

---
+++ index.js
@@ -0,0 +1,34 @@
+const aws = require('aws-sdk');
+const s3 = new aws.S3({ apiVersion: '2006-03-01' });
+const xlsx = require('xlsx');
+
+exports.handler = async (event) => {
+  const environment = event.queryStringParameters.environment;
+  const program_code = event.queryStringParameters.program_code;
+
+  // Fetch the excel file from the appropriate S3 bucket.
+  const file = await s3.getObject({ Bucket: environment, Key: 'bls-data.xlsx' }).promise();
+
+  // Read all data from the excel file.
+  const workbook = xlsx.read(file.Body);
+
+  // Get the individual sheets for each data source.
+  const all_job_outlooks = xlsx.utils.sheet_to_json(workbook.Sheets['Employment']);
+  const all_job_titles = xlsx.utils.sheet_to_json(workbook.Sheets['Job Titles']);
+
+  // Remove all data except the program we're looking for.
+  const filtered_job_outlooks = all_job_outlooks.filter(row => row.prospect_code === program_code);
+  const filtered_job_titles = all_job_titles.filter(row => row.prospect_code === program_code);
+
+  // Reduce the job titles data structure to something more usable.
+  const reduced_job_titles = filtered_job_titles.reduce((accumulator, current) => accumulator.concat([current]), []);
+
+  return {
+    statusCode: 200,
+    body: {
+      job_outlooks: filtered_job_outlooks,
+      job_titles: reduced_job_titles,
+    }
+  };
+
+};
